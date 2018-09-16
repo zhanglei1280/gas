81b2fef203966f80b657ab537a4b486f80de1b5c
zhanglei <1280368848@qq.com>
2018-09-16 22:10:04 +0200

    info 705 TP1 code

---
+++ INFO705/TP/TP1/code/calcultor/socketjava/Client.java
@@ -0,0 +1,43 @@
+package calcultor.socketjava;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import calculator.generic.ExpressionInvalide;
+
+public class Client {
+
+	public static void main(String[] args) throws IOException, ExpressionInvalide {
+	
+		String host = "127.0.0.1"; 
+	    int port = 3200;
+	    // 
+	    @SuppressWarnings("resource")
+		Socket S = new Socket(host, port);
+	    
+	    String expression = "3+2";
+	    System.out.println("<< Client : expression : " + expression);
+	    //String expression2 = "(4 - 3.14) * (2 - 5)";
+		
+	    OutputStream OS = S.getOutputStream();
+		PrintWriter PW = new PrintWriter(OS);
+		PW.println(expression);
+		PW.flush();
+		System.out.println("<< Client : envoyer " + expression);
+		
+		InputStream IS = S.getInputStream();
+		InputStreamReader ISR = new InputStreamReader(IS);
+		BufferedReader BR = new BufferedReader(ISR);
+		
+		String resultat = BR.readLine();
+		
+		System.out.println("<< Client : resultat : " + resultat);
+		
+	}
+
+}
---
+++ INFO705/TP/TP1/code/lanceur.java
@@ -0,0 +1,15 @@
+import calculator.generic.Calculatrice;
+import calculator.generic.MoteurCalculSimple;
+
+public class lanceur {
+
+	public static void main(String[] args) {
+		
+			MoteurCalculSimple m = new MoteurCalculSimple();
+			Calculatrice c = new Calculatrice(m);
+
+		
+
+	}
+
+}
