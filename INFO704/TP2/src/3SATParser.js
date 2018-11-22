2764b7261f6bdea08029307e4ba0ebb9cd1fd885
yan <i@yinyan.fr>
2018-11-22 17:44:35 +0100

    mid save

--- INFO704/TP2/src/3SATParser.js
+++ INFO704/TP2/src/3SATParser.js
@@ -6,18 +6,21 @@ const {
 
 const read3SAT = pathFile => new Promise((resolve, reject) => {
     fs.readFile(pathFile, (err, data) => {
-        if(err) reject(err)
+        if (err) reject(err)
         else resolve(data.toString())
     })
 })
 
 const abs = n => {
-    if(n >= 0) return n.toString()
-    return (-n).toString()
+    n = parseInt(n)
+    if (n >= 0) return JSON.stringify(n)
+    return JSON.stringify(-n)
 }
 
 const countVar = sat => {
-    const {clauses} = sat
+    const {
+        clauses
+    } = sat
     const vars = clauses.reduce((m, n) => m.concat(n))
     return uniq(vars.map(abs)).length
 }
@@ -28,17 +31,17 @@ const verify3SAT = sat => {
         nbClause,
         clauses
     } = sat
+    console.log(countVar(sat))
 
     return (
-        nbVar === countVar(sat)
-        &&
-        nbClause === clauses.length
+        nbVar == countVar(sat) &&
+        nbClause == clauses.length
     )
 }
 
 const parse3SAT = data => {
     // ensure 3SAT{...}
-    if(!/^3SAT{[^}]+}/.test(data)){
+    if (!/^3SAT{[^}]+}/.test(data)) {
         return Promise.reject("not proper 3SAT format.")
     }
     data = data.replace(/^3SAT{\n*/, "").replace(/}.*/, "").split("\n")
@@ -51,18 +54,79 @@ const parse3SAT = data => {
         clauses
     }
 
-    if(!verify3SAT(sat)){
+    if (!verify3SAT(sat)) {
         return Promise.reject("not a correct 3SAT.")
     }
     return sat
 }
 
 const SATGraph = sat => {
-    
+    var links = []
+    var points = []
+    var pClauses = []
+    var pTransition = ["t0"]
+    var pVars = []
+    const {
+        nbVar,
+        nbClause,
+        clauses
+    } = sat
+    for (let i = 0; i < nbClause; i++) {
+        pClauses.push(`c${i}`)
+    }
+    for (let i = 0; i < nbVar; i++) {
+        pTransition.push(`t${i+1}`)
+        let tmp = []
+        for (let j = 0; j < nbClause * 3 + 3; j++) {
+            tmp.push(`v_${i}_${j}`)
+        }
+        pVars.push(tmp)
+    }
+    points = pClauses.concat(pTransition)
+    for (let i of pVars) {
+        points.concat(i)
+    }
+    for (let i = 0; i < nbVar; i++) {
+        links.push([pTransition[i], pVars[i][0]])
+        links.push([pTransition[i], pVars[i][pVars[i].length - 1]])
+        links.push([pVars[i][0], pTransition[i + 1]])
+        links.push([pVars[i][pVars[i].length - 1], pTransition[i + 1]])
+        for (let j = 0; j < pVars[i].length - 1; j++) {
+            links.push([pVars[i][j], pVars[i][j + 1]])
+            links.push([pVars[i][j + 1], pVars[i][j]])
+        }
+    }
+    for(let i = 0; i < nbClause; i++){
+        for(let v of clauses[i]){
+            if(v > 0){
+                links.push([pVars[parseInt(v)-1][3*i+2], pClauses[i]])
+                links.push([pClauses[i], pVars[parseInt(v)-1][3*i+3]])
+            }else{
+                links.push([pClauses[i], pVars[-parseInt(v)-1][3*i+2]])
+                links.push([pVars[-parseInt(v)-1][3*i+3], pClauses[i]])
+            }
+        }
+    }
+    points = points.concat(pVars.reduce((m, n) => m.concat(n)))
+    return {
+        points,
+        links
+    }
 }
 
-read3SAT("../instances/3SAT_vrai.txt")
-    .then(parse3SAT)
-    .then(data => {
-        console.log(countVar(data))
-    })
+// read3SAT("../instances/3SAT_vrai.txt")
+//     .then(parse3SAT)
+//     .then(SATGraph)
+//     .then(graph => {
+//         console.log(graph)
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })
+
+const parser = pathFile =>
+    read3SAT(pathFile)
+        .then(parse3SAT)
+        .then(SATGraph)
+
+module.exports = parser
