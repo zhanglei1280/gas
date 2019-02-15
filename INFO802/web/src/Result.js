ad9db80bfca9ed826799f3746806240af90f68db
yan <i@yinyan.fr>
2019-02-15 10:46:33 +0100

    correspondance display opt

--- INFO802/web/src/Result.js
+++ INFO802/web/src/Result.js
@@ -20,41 +20,46 @@ class Result extends Component{
                     </span>
                     </a>
                 </header>
-                <div className="card-content">
-                    <div className="content">
+                <nav className="card-content">
+                    <nav className="content">
+                    {legs.length > 1 ? `${legs.length} correspondances` : "Direct"}
+                    <nav className="level is-mobile">
+                        <div className="level-left">
+                            <div className="level-item">
                         {
-                            legs.map((e, i) => (
-                                <div key={i}>
-                                <nav className="level is-mobile">
-                                    <div className="level-left">
-                                        <div className="level-item">
-                                        <h1>
-                                        {
-                                            moment(e.departure).format("HH:mm")
-                                        }
-                                        </h1>
-                                        </div>
-                                    </div>
-
-                                    <div className="level-right">
+                            // legs.map((e, i) => (
+                            //     <div key={i}>
+                            //             <h1>
+                            //             {
+                            //                 moment(e.departure).format("HH:mm")
+                            //             }
+                            //             &nbsp;
+                            //             {
+                            //                 i < legs.length - 1
+                            //                 ? <i className="fas fa-arrow-right"></i>
+                            //                 : ""
+                            //             }
+                            //             &nbsp;
+                            //             </h1>  
+                            //     </div>
+                            // ))
+                            <h1>{moment(legs[0].departure).format("HH:mm")}</h1>
+                        }
+                        </div>
+                        </div>
+                        <div className="level-right">
                                         <div className="level-item">
                                             <h1>{price} €</h1>
                                         </div>
                                     </div>
-                                
-                                </nav>
-                            </div>
-                                
-
-                            ))
-                        }
-                    </div>
-                </div>
+                    </nav>
+                </nav>
+                </nav>
                 <footer className="card-footer">
                     <a href="#" className="card-footer-item">Reserve your ticket</a>
                     <a href="#" className="card-footer-item">See on map</a>
                 </footer>
-                </div>
+            </div>
             </div>
         )
     }
