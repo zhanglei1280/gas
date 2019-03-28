c3889ec4ee229d37977bbfc41b2f5e73f96067e5
yan <i@yinyan.fr>
2019-03-28 10:46:01 +0100

    gas station

---
+++ gas/src/main/java/Hello.java
@@ -0,0 +1,16 @@
+import org.jspace.FormalField;
+import org.jspace.SequentialSpace;
+import org.jspace.Space;
+
+public class Hello {
+
+    public static void main(String[] argv) throws InterruptedException {
+        Space inbox = new SequentialSpace();
+
+        inbox.put("Hello World!");
+        Object[] tuple = inbox.get(new FormalField(String.class));
+        System.out.println(tuple[0]);
+
+    }
+
+}
